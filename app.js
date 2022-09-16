2b8b4ffa654f93d7ea090cfc62363dc258de29f9
nikeda <4104cafeaulait@gmail.com>
2022-09-17 01:22:34 +0900

    20220917-2

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var helloRouter = require('./routes/hello');
+var helloViewsRouter = require('./routes/views/hello');
+var samplesApiRouter = require('./routes/api/samples');  
 
 var app = express();
 
@@ -22,7 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hello', helloRouter);
+app.use('/hello', helloViewsRouter);
+app.use('/api/v1/samples', samplesApiRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
