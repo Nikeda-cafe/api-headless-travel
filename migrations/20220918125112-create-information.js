e5a159b13e69c45cd65ef7eea62f270b80ca7f84
nikeda <4104cafeaulait@gmail.com>
2022-09-19 00:14:13 +0900

    20220919-1

---
+++ migrations/20220918125112-create-information.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('information', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      group_id: {
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      status: {
+        type: Sequelize.INTEGER
+      },
+      emergency_flag: {
+        type: Sequelize.INTEGER
+      },
+      delete_flag: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('information');
+  }
+};
--- routes/views/information/index.js
+++ routes/views/information/index.js
@@ -1,11 +1,38 @@
 var express = require('express');
-// const db = require('../../models');
+const db = require('../../../models');
 var router = express.Router();
 
 /* GET users listing. */
+
+// list 
 router.get('/', function(req, res, next) {
     const data = {}
     res.render('information/index',data)
+});
+
+// regist
+router.get('/regist', function(req, res, next) {
+    const msg = db.information.returnHello();
+    console.log(msg);
+    const data = {}
+    res.render('information/regist',data)
+});
+
+router.post('/regist', function(req, res, next) {
+    // console.log(req.body['title']); 
+    const data = {}
+    res.render('information/regist',data)
+});
+
+// edit
+router.get('/edit', function(req, res, next) {
+    const data = {}
+    res.render('information/edit',data)
+});
+
+router.post('/edit', function(req, res, next) {
+    const data = {}
+    res.render('information/edit',data)
 });
 
 module.exports = router;
