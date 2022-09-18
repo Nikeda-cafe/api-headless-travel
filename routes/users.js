89bf18204f224992e42d9014275cb255afe054d4
nikeda <4104cafeaulait@gmail.com>
2022-09-18 17:59:23 +0900

    20220918-6

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  db.User.findAll().then(users => {
-    const data = {
-      title: 'Users/Index',
-      content: users
-    }
-    res.render('users/index',data)
-  })
+    const conditionId = req.query.id;
+    const attributes = { attributes: ["id", "name", "mail", "age", ["DATE_FORMAT(createdAt, '%Y年%m月%d日')", "createdAt"]] }
+    const wheres = conditionId ? { where: { id: conditionId } } : {}
+    const conditions = Object.assign(attributes,wheres)
+    db.User.findAll(conditions).then(users => {
+        const data = {
+            title: 'Users/Index',
+            content: users
+        }
+        res.render('users/index',data)
+    })
 });
 
 module.exports = router;
