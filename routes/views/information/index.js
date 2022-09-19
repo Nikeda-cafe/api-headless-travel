82b03e1c8a4abc802b785cedb5ca1547577dff2d
nikeda <4104cafeaulait@gmail.com>
2022-09-19 17:46:38 +0900

    20220919-6

--- routes/views/information/index.js
+++ routes/views/information/index.js
@@ -2,6 +2,17 @@ var express = require('express');
 const db = require('../../../models');
 var router = express.Router();
 
+// sequelize オブジェクト呼び出し
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../../../config/config.json')[env];
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
 /* GET users listing. */
 
 // list 
@@ -10,31 +21,92 @@ router.get('/', function(req, res, next) {
         title:'お知らせ一覧',
         h1: 'お知らせ一覧',
     }
-    const conditionId = req.query.id;
-    const attributes = { 
-        attributes: ["id", "group_id", "title", "status", "emergency_flag", ["DATE_FORMAT(createdAt, '%Y年%m月%d日')", "createdAt"]] 
+
+    const postData = {
+        id: '',
+        category:'',
+        status: '',
+        emergency: ''
     }
-    const wheres = conditionId ? { where: { id: conditionId } } : {}
-    const conditions = Object.assign(attributes,wheres)
-    db.information.findAll(conditions).then(result => {
-        const data = {
-            list: result,
-            metaData: metaData
+
+    // columns
+    const columns = ["i.id", "ic.name as category", "i.title", "i.status", "i.emergency_flag", "DATE_FORMAT(i.createdAt, '%Y年%m月%d日') as createdAt"] 
+    // join
+    const join = ["inner join information_categories as ic on i.group_id = ic.id"]
+    sequelize.query(`select ${columns.join(',')} from information as i ${join.join(' ')}`)
+        .then(result => {
+            const data = {
+                metaData:metaData,
+                list: result[0],
+                postData: postData
+            }
+            res.render('information/index',data)
         }
-        res.render('information/index',data)
-    })
+    );
+});
+
+// list 
+router.post('/', function(req, res, next) {
+
+    const id = req.body.id ? req.body.id : ''
+    const category = req.body.category ? req.body.category : ''
+    const status = req.body.status ? req.body.status : ''
+    const emergency = req.body.emergency === 'on' ? 1 : ''
+    const postData = {
+        id: id,
+        category:category,
+        status: status,
+        emergency: emergency
+    }
+
+    const metaData = {
+        title:'お知らせ一覧',
+        h1: 'お知らせ一覧',
+    }
+
+    // where
+    const where = ["1 = 1"]
+    if(id !== ''){
+        where.push(`i.id = ${id}`)  
+    }
+    if(category !== ''){
+        where.push(`i.group_id = ${category}`)
+    }
+    if(status !== ''){
+        where.push(`i.status = ${status}`)
+    }
+    if(emergency !== ''){
+        where.push(`i.emergency_flag = ${emergency}`)
+    }
+    // columns
+    const columns = ["i.id", "ic.name as category", "i.title", "i.status", "i.emergency_flag", "DATE_FORMAT(i.createdAt, '%Y年%m月%d日') as createdAt"] 
+    // join
+    const join = ["inner join information_categories as ic on i.group_id = ic.id"]
+    sequelize.query(`SELECT ${columns.join(',')} FROM information as i ${join.join(' ')} WHERE ${where.join(' AND ')}`)
+        .then(result => {
+            const data = {
+                metaData:metaData,
+                list: result[0],
+                postData: postData
+            }
+            res.render('information/index',data)
+        }
+    );
 });
 
 // regist
 router.get('/regist', function(req, res, next) {
-    const msg = db.information.returnHello();
-    console.log(msg);
-    const data = {}
+    const metaData = {
+        title:'お知らせ登録',
+        h1: 'お知らせ登録',
+    }
+    const data = {
+        metaData:metaData
+    }
     res.render('information/regist',data)
 });
 
 router.post('/regist', function(req, res, next) {
-    console.log(req.body); 
     db.sequelize.sync().then(() => {
         db.information.create({
             title: req.body.title,
@@ -53,18 +125,52 @@ router.post('/regist', function(req, res, next) {
 
 // edit
 router.get('/edit', function(req, res, next) {
-    const data = {}
-    res.render('information/edit',data)
+    const queryId = req.query.id ? req.query.id : ''
+    db.information.findAll({ where: { id: queryId } }).then(result => {
+        const metaData = {
+            title:'お知らせ情報編集',
+            h1: 'お知らせ情報編集',
+        }
+        const data = {
+            metaData: metaData,
+            post: result[0]
+        }
+        res.render('information/edit',data)
+    })
+    
+    
 });
 
 router.post('/edit', function(req, res, next) {
-    const data = {}
-    res.render('information/edit',data)
+    const queryId = req.query.id ? req.query.id : ''
+    db.sequelize.sync().then(() => {
+        db.information.update(
+            {
+                title: req.body.title,
+                group_id: req.body.group,
+                content: req.body.content,
+                status: req.body.status,
+                emergency_flag: req.body.emergency ? 1 : 0,
+                
+            },
+            {
+                where: { id: queryId }
+            }
+        ).then(model => {
+            res.redirect('/information/complete')
+        })
+    })
 });
 
 // complete
 router.get('/complete', function(req, res, next) {
-    const data = {}
+    const metaData = {
+        title:'お知らせ登録完了',
+        h1: 'お知らせ登録完了',
+    }
+    const data = {
+        metaData: metaData
+    }
     res.render('information/complete',data)
 });
 
