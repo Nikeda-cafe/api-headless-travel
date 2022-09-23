adc600339a88e6d6caf89391ccf494a918fabc57
nikeda <4104cafeaulait@gmail.com>
2022-09-23 16:15:57 +0900

    cdc

--- public/js/base.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.navbar-toggler').onclick = function() {
-    // if menu is opening, close menu
-    if(document.getElementById('navbar').classList.contains('show')) {
-        document.querySelector('#navbar').classList.remove('show');
-    } else {
-      document.querySelector('#navbar').classList.add('show');
-    }
-}
