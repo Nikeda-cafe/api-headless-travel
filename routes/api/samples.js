4b05b2f84904db760fcd2172ad7cfc8e253ab52c
nikeda <4104cafeaulait@gmail.com>
2022-09-24 03:46:52 +0900

    csa

--- routes/api/samples.js
+++ routes/api/samples.js
@@ -6,30 +6,10 @@ var router = express.Router();
  * JSON形式で文字列を返す。
  */
 
-var { Client } = require('pg');
-var client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'my_db',
-    password: 'postgres',
-    port: 5432
-})
- 
-client.connect()
+
 
 router.get('/', function(req, res, next) {
-    const query = {
-        text: 'select * from information_categories',
-    }
-    client.query(query, (error, result) => {
-        if(error){
-
-        }else{
-            res.header('Content-Type', 'application/json; charset=utf-8')
-            res.send(result.rows);
-        }
-      }
-    );
+    
 });
 
 
